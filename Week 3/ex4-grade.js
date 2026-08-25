3ec626b0bbbec4b321cbe4606d0746a2f8d29ca5
0xA1525A <natthakit.twc@gmail.com>
2026-08-25 20:45:05 +0700

    refactored ex4-grade.js and applied n-tier architecture style.

--- Week 3/ex4-grade.js
+++ Week 3/ex4-grade.js
@@ -1,55 +1,75 @@
-function isScoreValid(score) {
-    if (typeof score !== typeof 0
-        || score < 0
-        || score > 100
-        || Number.isNaN(score)
-        || !Number.isFinite(score)
-    ) {
-        return false;
+const SYS_CONFIGS = {
+    GRADE_MIN_SCORE: {
+        A:      80,
+        B_PLUS: 75,
+        B:      70,
+        C_PLUS: 65,
+        C:      60,
+        D_PLUS: 55,
+        D:      50
     }
-    return true;
-}
+};
 
-function labelGradeByScore(score) {
-    if (score >= 80) {
-        return "A";
-    }
-    if (score >= 75) {
-        return "B+";
-    }
-    if (score >= 70) {
-        return "B";
-    }
-    if (score >= 65) {
-        return "C+";
-    }
-    if (score >= 60) {
-        return "C";
-    }
-    if (score >= 55) {
-        return "D+";
-    }
-    if (score >= 50) {
-        return "D";
-    }
-    return "F";
-}
+const GradeService = class {
+    static isScoreValid = function(score) {
+        if (typeof score !== "number"
+            || score < 0
+            || score > 100
+            || Number.isNaN(score)
+            || !Number.isFinite(score)
+        ) {
+            return false;
+        }
+        return true;
+    };
 
-function getGrade(score) {
-    if (!isScoreValid(score)) {
-        return undefined;
-    }
+    static grade = function(score) {
+        if (!this.isScoreValid(score)) {
+            return "Invalid";
+        }
 
-    const grade = labelGradeByScore(score);
+        // this is sorted in descending way because otherwise-
+        // you will always get a d.
+        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.A) {
+            return "A";
+        }
+        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.B_PLUS) {
+            return "B+";
+        }
+        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.B) {
+            return "B";
+        }
+        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.C_PLUS) {
+            return "C+";
+        }
+        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.C) {
+            return "C";
+        }
+        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.D_PLUS) {
+            return "D+";
+        }
+        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.D) {
+            return "D";
+        }
+        return "F";
+    };
+};
 
-    return grade;
-}
+const GradeView = class {
+    static toGrade = function(score) {
+        const grade = GradeService.grade(score);
+
+        return grade;
+    };
+};
 
 (function() {
+    // test cases assign by the assignment.
     const scoreTestCases = [95, 80, 79, 75, 70, 65, 60, 55, 50, 49, 0, -5, 120];
 
     for (const score of scoreTestCases) {
-        const grade = getGrade(score);
-        console.log(`Score: ${score}\t=> ${grade} grade.`);
+        const grade = GradeView.toGrade(score);
+
+        console.log(`Score: ${score}\t=> ${grade}`);
     }
 })();
