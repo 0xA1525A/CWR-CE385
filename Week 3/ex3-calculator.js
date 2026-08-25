9d72d998bfbeb92a2273e8aa7f86d42fae797d58
0xA1525A <natthakit.twc@gmail.com>
2026-08-25 20:20:54 +0700

    refactored ex3-calculator.js and applied n-tier architecture style.

--- Week 3/ex3-calculator.js
+++ Week 3/ex3-calculator.js
@@ -1,54 +1,141 @@
-const WORKSHOP_FULL_MARK   = 60;
-const WORKSHOP_SCORE_RATIO = 20;
-const PERCENT_RATIO        = 100;
-const TOTAL_FULL_MARK      = 100;
-const N_MARK_COMPARISON_TARGET = 80;
+const SYS_CONFIGS = {
+    FULL_RAW_WORKSHOP_SCORE: 60,
+    FULL_RESCALRED_WORKSHOP_SCORE: 20,
+    FULL_TOTAL_SCORE:    100,
+    PERCENT_DENOMINATOR: 100,
+    MAX_DECIMAL_POINTS:  2,
+    COMPARISON_SCORE_TARGET: 80,
+    VALID_STUDENT_ID_LENGTH: 8,
+    VALID_SUBJECT_ID_LENGTH: 5
+};
 
-function calculateMarkRatio(score, denominator, multiplier) {
-    const N_DECIMAL_POINT = 2;
+const CEScoreDB = class {
+    static CE385 = {
+            "69674200": {
+                workshop:   48,
+                attendance: 9,
+                project:    17,
+                midterm:    15,
+                final:      24
+        }
+    };
+};
 
-    return Number((score / denominator * multiplier).toFixed(N_DECIMAL_POINT));
-}
+const ScoreRepository = class {
+    // this is intentional - as it returns undefined if `id` not found.
+    static getScoreById = function(subjectId, studentId) {
+        return CEScoreDB[subjectId]?.[studentId];
+    };
+};
 
-function calculateTotalMark(achievedMark, ratioedWorkshopMark) {
-    return ratioedWorkshopMark + achievedMark.attendance + achievedMark.project + achievedMark.midterm + achievedMark.final;
-}
-
-function calculateNMarkToTarget(target, score) {
-    return target - score;
-}
-
-function formatMarkSummary(achievedRawMark, totalMark, totalMarkPercent, nMarkToTarget, ratioedWorkshopMark) {
-    return `\
-    [ SCORE CALCULATOR ] ------
-    # TOTAL MARK: ${totalMark}/${TOTAL_FULL_MARK} (${totalMarkPercent}%)
-    |- Ratio'd Workshop Mark:\t${ratioedWorkshopMark}/${WORKSHOP_SCORE_RATIO}
-    :  |- Raw Workshop Mark:\t${achievedRawMark.workshop}/${WORKSHOP_FULL_MARK}
-    |- Attendance Mark:\t\t${achievedRawMark.attendance}
-    |- Project Mark:\t\t${achievedRawMark.project}
-    |- Midterm Mark:\t\t${achievedRawMark.midterm}
-    |- Final Mark:\t\t${achievedRawMark.final}
-    :
-    |- N-Mark to ${N_MARK_COMPARISON_TARGET}:\t\t${nMarkToTarget}
-    `;
-}
-
-// main function:
-(function() {
-    const achievedRawMark = {
-        workshop:   48,
-        attendance: 9,
-        project:    17,
-        midterm:    15,
-        final:      24
+const ScoreService = class {
+    static rescaleScore = function(score, denominator, multiplier) {
+        return Number((score / denominator * multiplier).toFixed(SYS_CONFIGS.MAX_DECIMAL_POINTS));
     };
 
-    const ratioedWorkshopMark = calculateMarkRatio(achievedRawMark.workshop, WORKSHOP_FULL_MARK, WORKSHOP_SCORE_RATIO);
-    const totalMark        = calculateTotalMark(achievedRawMark, ratioedWorkshopMark);
-    const totalMarkPercent = calculateMarkRatio(totalMark, TOTAL_FULL_MARK, PERCENT_RATIO);
-    const nMarkToTarget    = calculateNMarkToTarget(N_MARK_COMPARISON_TARGET, totalMark);
-    const formattedMessage = formatMarkSummary(achievedRawMark, totalMark, totalMarkPercent, nMarkToTarget, ratioedWorkshopMark);
+    static calculateSumOfScores = function(score, scaledWorkshop) {
+        return score.attendance + score.project + score.midterm + score.final + scaledWorkshop;
+    };
 
-    console.log("[PART 3: DISPLAY CALCULATED VALUES]");
-    console.log(formattedMessage);
+    static calculateDifference = function(target, score) {
+        return target - score;
+    };
+
+    static isSubjectIdValid = function(id) {
+        return (
+            typeof id === "string"
+        &&  id.length === SYS_CONFIGS.VALID_SUBJECT_ID_LENGTH
+        &&  id.trim() === id
+        );
+    };
+
+    static isStudentIdValid = function(id) {
+        return (
+            typeof id === "string"
+        &&  id.length === SYS_CONFIGS.VALID_STUDENT_ID_LENGTH
+        &&  id.trim() === id
+        );
+    };
+
+    static getScoreSummary(subjectId, studentId) {
+        if (!this.isSubjectIdValid(subjectId)) {
+            return { success: false, message: "Invalid Subject ID" };
+        }
+
+        if (!this.isStudentIdValid(studentId)) {
+            return { success: false, message: "Invalid Student ID" };
+        }
+
+        const score = ScoreRepository.getScoreById(subjectId, studentId);
+        if (!score) {
+            return { success: false, message: "Score Not Found" };
+        }
+
+        // calculate x out of z when x out of y; while the actual score is 20 out of z.
+        const scaledWorkshop = this.rescaleScore(score.workshop, SYS_CONFIGS.FULL_RAW_WORKSHOP_SCORE, SYS_CONFIGS.FULL_RESCALRED_WORKSHOP_SCORE);
+
+        const total = this.calculateSumOfScores(score, scaledWorkshop);
+        const totalInPercent = this.rescaleScore(total, SYS_CONFIGS.PERCENT_DENOMINATOR, SYS_CONFIGS.FULL_TOTAL_SCORE);
+
+        // leave negative value as is as the assignment says.
+        const targetDifference = this.calculateDifference(SYS_CONFIGS.COMPARISON_SCORE_TARGET, total);
+
+        const summary = {
+            success: true,
+            total: total,
+            totalInPercent: totalInPercent,
+            scaledWorkshop: scaledWorkshop,
+            rawWorkshop: score.workshop,
+            attendance: score.attendance,
+            project: score.project,
+            midterm: score.midterm,
+            final: score.final,
+            targetDifference: targetDifference
+        };
+
+        return summary;
+    };
+};
+
+const FormatterService = class {
+    static removeTemplateSourceIndent = function(message) {
+        return message.replace(/^[ \t]+/gm, '');
+    };
+
+    static formatSummary(summary) {
+        const template = this.removeTemplateSourceIndent(`\
+            [ SCORE CALCULATOR ] =>-------------
+            # TOTAL MARK: ${summary.total}/${SYS_CONFIGS.FULL_TOTAL_SCORE} (${summary.totalInPercent}%)
+            |- Ratio'd Workshop Mark:\t${summary.scaledWorkshop}/${SYS_CONFIGS.FULL_RESCALRED_WORKSHOP_SCORE}
+            :  |- Raw Workshop Mark:\t${summary.rawWorkshop}/${SYS_CONFIGS.FULL_RAW_WORKSHOP_SCORE}
+            |- Attendance Mark:\t\t${summary.attendance}
+            |- Project Mark:\t\t${summary.project}
+            |- Midterm Mark:\t\t${summary.midterm}
+            |- Final Mark:\t\t\t${summary.final}
+            :
+            |- N-Mark to ${SYS_CONFIGS.COMPARISON_SCORE_TARGET}:\t\t${summary.targetDifference}\
+        `);
+
+        return template;
+    };
+};
+
+const ScoreView = class {
+    static getFormattedSummaryByIds = function(subjectId, studentId) {
+        const summary = ScoreService.getScoreSummary(subjectId, studentId);
+        if (!summary.success) {
+            return summary.message;
+        }
+
+        const formattedSummary = FormatterService.formatSummary(summary);
+
+        return formattedSummary;
+    };
+};
+
+// this simulates function call from another function.
+(function() {
+    const summary = ScoreView.getFormattedSummaryByIds("CE385", "69674200");
+
+    console.log(summary);
 })();
