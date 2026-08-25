4741682641dec006d2ddb6fa723f23f867835ab1
0xA1525A <natthakit.twc@gmail.com>
2026-08-25 20:47:34 +0700

    moved things a little to make it easier to read

--- Week 3/ex4-grade.js
+++ Week 3/ex4-grade.js
@@ -1,15 +1,3 @@
-const SYS_CONFIGS = {
-    GRADE_MIN_SCORE: {
-        A:      80,
-        B_PLUS: 75,
-        B:      70,
-        C_PLUS: 65,
-        C:      60,
-        D_PLUS: 55,
-        D:      50
-    }
-};
-
 const GradeService = class {
     static isScoreValid = function(score) {
         if (typeof score !== "number"
@@ -28,27 +16,39 @@ const GradeService = class {
             return "Invalid";
         }
 
+        // move things together to make it easier-
+        // to change things later.
+        const GRADE_MIN_SCORE = {
+            A:      80,
+            B_PLUS: 75,
+            B:      70,
+            C_PLUS: 65,
+            C:      60,
+            D_PLUS: 55,
+            D:      50
+        };
+
         // this is sorted in descending way because otherwise-
         // you will always get a d.
-        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.A) {
+        if (score >= GRADE_MIN_SCORE.A) {
             return "A";
         }
-        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.B_PLUS) {
+        if (score >= GRADE_MIN_SCORE.B_PLUS) {
             return "B+";
         }
-        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.B) {
+        if (score >= GRADE_MIN_SCORE.B) {
             return "B";
         }
-        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.C_PLUS) {
+        if (score >= GRADE_MIN_SCORE.C_PLUS) {
             return "C+";
         }
-        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.C) {
+        if (score >= GRADE_MIN_SCORE.C) {
             return "C";
         }
-        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.D_PLUS) {
+        if (score >= GRADE_MIN_SCORE.D_PLUS) {
             return "D+";
         }
-        if (score >= SYS_CONFIGS.GRADE_MIN_SCORE.D) {
+        if (score >= GRADE_MIN_SCORE.D) {
             return "D";
         }
         return "F";
