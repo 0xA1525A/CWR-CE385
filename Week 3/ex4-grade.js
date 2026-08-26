05a38ed82d3fcc746d6eb6d66564216b886778b4
0xA1525A <natthakit.twc@gmail.com>
2026-08-26 08:32:12 +0700

    removed unnecessary n-tier architecture this ex4-grade.js

--- Week 3/ex4-grade.js
+++ Week 3/ex4-grade.js
@@ -1,66 +1,56 @@
-const GradeService = class {
-    static isScoreValid = function(score) {
-        if (typeof score !== "number"
-            || score < 0
-            || score > 100
-            || Number.isNaN(score)
-            || !Number.isFinite(score)
-        ) {
-            return false;
-        }
-        return true;
-    };
-
-    static grade = function(score) {
-        if (!this.isScoreValid(score)) {
-            return "Invalid";
-        }
-
-        // move things together to make it easier-
-        // to change things later.
-        const GRADE_MIN_SCORE = {
-            A:      80,
-            B_PLUS: 75,
-            B:      70,
-            C_PLUS: 65,
-            C:      60,
-            D_PLUS: 55,
-            D:      50
-        };
-
-        // this is sorted in descending way because otherwise-
-        // you will always get a d.
-        if (score >= GRADE_MIN_SCORE.A) {
-            return "A";
-        }
-        if (score >= GRADE_MIN_SCORE.B_PLUS) {
-            return "B+";
-        }
-        if (score >= GRADE_MIN_SCORE.B) {
-            return "B";
-        }
-        if (score >= GRADE_MIN_SCORE.C_PLUS) {
-            return "C+";
-        }
-        if (score >= GRADE_MIN_SCORE.C) {
-            return "C";
-        }
-        if (score >= GRADE_MIN_SCORE.D_PLUS) {
-            return "D+";
-        }
-        if (score >= GRADE_MIN_SCORE.D) {
-            return "D";
-        }
-        return "F";
-    };
+function isScoreValid(score) {
+    if (typeof score !== "number"
+        || score < 0
+        || score > 100
+        || Number.isNaN(score)
+        || !Number.isFinite(score)
+    ) {
+        return false;
+    }
+    return true;
 };
 
-const GradeView = class {
-    static toGrade = function(score) {
-        const grade = GradeService.grade(score);
+function toGrade(score) {
+    if (!isScoreValid(score)) {
+        return "Invalid";
+    }
 
-        return grade;
+    // move things together to make it easier-
+    // to change things later.
+    const GRADE_MIN_SCORE = {
+        A:      80,
+        B_PLUS: 75,
+        B:      70,
+        C_PLUS: 65,
+        C:      60,
+        D_PLUS: 55,
+        D:      50
     };
+
+    // this is sorted in descending way because otherwise-
+    // you will always get a d.
+    if (score >= GRADE_MIN_SCORE.A) {
+        return "A";
+    }
+    if (score >= GRADE_MIN_SCORE.B_PLUS) {
+        return "B+";
+    }
+    if (score >= GRADE_MIN_SCORE.B) {
+        return "B";
+    }
+    if (score >= GRADE_MIN_SCORE.C_PLUS) {
+        return "C+";
+    }
+    if (score >= GRADE_MIN_SCORE.C) {
+        return "C";
+    }
+    if (score >= GRADE_MIN_SCORE.D_PLUS) {
+        return "D+";
+    }
+    if (score >= GRADE_MIN_SCORE.D) {
+        return "D";
+    }
+    return "F";
 };
 
 (function() {
@@ -68,7 +58,7 @@ const GradeView = class {
     const scoreTestCases = [95, 80, 79, 75, 70, 65, 60, 55, 50, 49, 0, -5, 120];
 
     for (const score of scoreTestCases) {
-        const grade = GradeView.toGrade(score);
+        const grade = toGrade(score);
 
         console.log(`Score: ${score}\t=> ${grade}`);
     }
